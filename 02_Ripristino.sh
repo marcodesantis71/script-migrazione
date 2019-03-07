a05dffd9e35baa4f6354eeb2cbb00e44a998d8c8
Marco <marco_desantis@icloud.com>
2019-03-07 10:41:56 +0100

    Inserito e testato recupero BCK e Installzione DNS

--- 02_Ripristino.sh
+++ 02_Ripristino.sh
@@ -6,7 +6,7 @@
 # 1) Check data
 # 2) Recupero BCK
 # 3) Installazione e Ripristino DHCP Server
-# 4) Personalizzazione Profile
+# 4) Installazione e Ripristino DNS Server
 # 5) Modifica configurazione rete, file hosts e hostname
 # 6) Update sistema
 # 7) Rimozione Firewall
@@ -53,6 +53,7 @@ fi
 
 ## FUNZIONE RECUPERO BCK ##
 function recupero_bck {
+echo "RECUPERO BCK $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
 scp -r -o StrictHostKeyChecking=no ${user_nas}@${ip_nas}:${path_servizi}/dhcp/dhcp_${data}.tar /home/thegod/
 scp -r -o StrictHostKeyChecking=no ${user_nas}@${ip_nas}:${path_servizi}/bind/bind_${data}.tar /home/thegod/
 scp -r -o StrictHostKeyChecking=no ${user_nas}@${ip_nas}:${path_mysql}/*_${data}.sql /home/thegod/
@@ -69,11 +70,13 @@ scp -r -o StrictHostKeyChecking=no ${user_nas}@${ip_nas}:${path_homebridge}/home
 ## FUNZIONE INSTALLAZIONE DHCP ##
 
 function install_dhcp {
+echo "INSTALLO DHCP $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
 apt-get install isc-dhcp-server -y
 }
 
 ## FUNZIONE GESTIONE FILE DI LOG DHCP ##
 function dhcp_log {
+echo "CREAZIONE LOG DHCP $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
 mkdir /var/log/dhcpd/
 chown -R syslog:adm /var/log/dhcpd
 echo "local7.debug    /var/log/dhcpd/dhcpd.log" >> /etc/rsyslog.d/10-fixed_ip.conf
@@ -82,6 +85,7 @@ sed -i "s/INTERFACESv4=\"\"/INTERFACESv4=\"eth0\"/g" /etc/default/isc-dhcp-serve
 
 ## FUNZIONE RIPRISTINO BCK DHCP ##
 function ripristino_dhcp {
+echo "RIPRISTINO DHCP $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
 tar -xvf dhcp_${data}.tar
 cp /home/thegod/etc/dhcp/dhcpd.conf /etc/dhcp/
 cp /home/thegod/etc/dhcp/fixed_ip.conf /etc/dhcp/
@@ -89,10 +93,37 @@ cp /home/thegod/etc/dhcp/fixed_ip.conf /etc/dhcp/
 
 ## FUNZIONE RESTART SERVIZI DHCP ##
 function restart_dhcp {
+echo "RIAVVIO DHCP $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
 systemctl restart rsyslog
 systemctl restart isc-dhcp-server
 }
 
+## FUNZIONE INSTALLAZIONE DNS ##
+function install_dns {
+echo "INSTALLO DNS $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
+apt-get install bind9 bind9utils bind9-doc -y
+}
+
+## FUNZIONE RIPRISTINO BCK DNS ##
+function ripristino_dns {
+echo "RIPRISTINO DNS $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
+tar -xvf bind_${data}.tar
+cp /home/thegod/etc/bind/* /etc/bind
+}
+
+## FUNZIONE MODIFICA RETE ##
+function modifica_dns {
+echo "MODIFICA DNS $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
+sed -i 's/8.8.8.8/127.0.0.1/g' /etc/netplan/50-cloud-init.yaml
+netplan apply
+}
+
+## FUNZIONE RIAVVIO SERVIZI DNS ##
+function restart_dns {
+echo "RIAVVIO DNS $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
+netplan apply
+systemctl restart bind9
+}
 #inizio_script
 #check_utente
 #check_data
@@ -101,3 +132,7 @@ systemctl restart isc-dhcp-server
 #dhcp_log
 #ripristino_dhcp
 #restart_dhcp
+#install_dns
+#ripristino_dns
+#modifica_dns
+#restart_dns
