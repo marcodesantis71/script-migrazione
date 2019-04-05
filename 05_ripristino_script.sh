d0dffc5a9b8726f52113f697ed9b4270ddd111ad
Marco de Santis <marco_desantis@icloud.com>
2019-04-05 11:10:54 +0200

    Ripristinati script crontab

--- 05_ripristino_script.sh
+++ 05_ripristino_script.sh
@@ -371,3 +371,141 @@ case \"\${1}\" in
 ;;
 esac
 } > /root/Script/bck.sh
+
+## FUNZIONE RIPRISTINO PULIZIA VIDEO ##
+function ripristino_pulizia_video {
+echo "Ripristino pulizia videosorveglianza $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/05_ripristino_script.log
+echo "#/bin/bash
+set -x
+source ~/.bashrc
+
+export OGGI2=\$(date +%d_%b_%Y)
+export RECEIVER=report@sistemiesistemi.it
+export EMAIL=report@sistemiesistemi.it
+
+
+function Elimina {
+                          ssh -oStrictHostKeyChecking=no  admin@qnap \"find /share/CACHEDEV1_DATA/homes/marco/VideoSorveglianza/ -mtime +35\" > /tmp/lista_del.log
+for i in \`cat /tmp/lista_del.log\`; do ssh admin@qnap \"rm  \$i\";done
+}
+
+function Invio_Mail {
+
+VIDEO_UNDERSTAIR=\`cat /tmp/lista_del.log | grep UnderStair | grep mkv | wc -l | awk '{print \$1}'\`
+VIDEO_KITCHEN=\`cat /tmp/lista_del.log | grep Kitchen | grep mkv | wc -l | awk '{print \$1}'\`
+VIDEO_OFFICE=\`cat /tmp/lista_del.log | grep Office | grep mkv | wc -l | awk '{print \$1}'\`
+VIDEO_EXTERNAL=\`cat /tmp/lista_del.log | grep External | grep mkv | wc -l | awk '{print \$1}'\`
+VIDEO_LEAVING=\`cat /tmp/lista_del.log | grep Leaving | grep mkv | wc -l | awk '{print \$1}'\`
+VIDEO_BEDROOM=\`cat /tmp/lista_del.log | grep BedRoom | grep mp4 | wc -l | awk '{print \$1}'\`
+SNAP_UNDERSTAIR=\`cat /tmp/lista_del.log | grep UnderStair | grep jpg | wc -l | awk '{print \$1}'\`
+SNAP_KITCHEN=\`cat /tmp/lista_del.log | grep Kitchen | grep jpg | wc -l | awk '{print \$1}'\`
+SNAP_OFFICE=\`cat /tmp/lista_del.log | grep Office | grep jpg | wc -l | awk '{print \$1}'\`
+SNAP_EXTERNAL=\`cat /tmp/lista_del.log | grep External | grep jpg | wc -l | awk '{print \$1}'\`
+SNAP_LEAVING=\`cat /tmp/lista_del.log | grep Leaving | grep jpg | wc -l | awk '{print \$1}'\`
+
+if [[ \$VIDEO_UNDERSTAIR == '0' ]] && [[ \$SNAP_UNDERSTAIR == '0' ]] ; then
+echo \"Oggi non ci sono stati file da eliminare.\" | mutt -s \"Report Pulizia Cam-Understair del \${OGGI2}\" \"\${RECEIVER}\"
+else
+echo \"Nella giornata odierna sono stati eliminati \$VIDEO_UNDERSTAIR video e \$SNAP_UNDERSTAIR immagini.\" | mutt -s \"Report Pulizia Cam-Understair del \${OGGI2}\" \"\${RECEIVER}\"
+fi
+
+if [[ \$VIDEO_LEAVING == '0' ]] && [[ \$SNAP_LEAVING == '0' ]] ; then
+echo \"Oggi non ci sono stati file da eliminare.\" | mutt -s \"Report Pulizia Cam-Leaving del \${OGGI2}\" \"\${RECEIVER}\"
+else
+echo \"Nella giornata odierna sono stati eliminati \$VIDEO_LEAVING video e \$SNAP_LEAVING immagini.\" | mutt -s \"Report Pulizia Cam-Leaving del \${OGGI2}\" \"\${RECEIVER}\"
+fi
+
+if [[ \$VIDEO_EXTERNAL == '0' ]] && [[ \$SNAP_EXTERNAL == '0' ]] ; then
+echo \"Oggi non ci sono stati file da eliminare.\" | mutt -s \"Report Pulizia Cam-External del \${OGGI2}\" \"\${RECEIVER}\"
+else
+echo \"Nella giornata odierna sono stati eliminati \$VIDEO_EXTERNAL video e \$SNAP_EXTERNAL immagini.\" | mutt -s \"Report Pulizia Cam-External del \${OGGI2}\" \"\${RECEIVER}\"
+fi
+
+if [[ \$VIDEO_OFFICE == '0' ]] && [[ \$SNAP_OFFICE == '0' ]] ; then
+echo \"Oggi non ci sono stati file da eliminare.\" | mutt -s \"Report Pulizia Cam-Office del \${OGGI2}\" \"\${RECEIVER}\"
+else
+echo \"Nella giornata odierna sono stati eliminati \$VIDEO_OFFICE video e \$SNAP_OFFICE immagini.\" | mutt -s \"Report Pulizia Cam-Office del \${OGGI2}\" \"\${RECEIVER}\"
+fi
+
+if [[ \$VIDEO_KITCHEN == '0' ]] && [[ \$SNAP_KITCHEN == '0' ]] ; then
+echo \"Oggi non ci sono stati file da eliminare.\" | mutt -s \"Report Pulizia Cam-Kitchen del \${OGGI2}\" \"\${RECEIVER}\"
+else
+echo \"Nella giornata odierna sono stati eliminati \$VIDEO_KITCHEN video e \$SNAP_KITCHEN immagini.\" | mutt -s \"Report Pulizia Cam-Kitchen del \${OGGI2}\" \"\${RECEIVER}\"
+fi
+
+if [[ \$VIDEO_BEDROOM == '0' ]] ; then
+echo \"Oggi non ci sono stati file da eliminare.\" | mutt -s \"Report Pulizia Cam-BedRoom del \${OGGI2}\" \"report@sistemiesistemi.it\" -e 'my_hdr From:report@sistemiesistemi.it'
+else
+echo \"Nella giornata odierna sono stati eliminati \$VIDEO_BEDROOM video.\" |  mutt -s \"Report Pulizia Cam-BedRoom del \${OGGI2}\" \"\${RECEIVER}\" -e 'my_hdr From:report@sistemiesistemi.it'
+fi
+}
+case \"\${1}\" in
+	pulisci)
+	Elimina
+	Invio_Mail
+	;;
+	invio)
+	Invio_Mail
+	;;
+        *)
+        echo \"UTILIZZO:{pulisci}\"
+        ;;
+esac " > /home/thegod/Script/Pulizia_VideoSorveglianza.sh
+}
+
+## FUNZIONE RIPRISTINO CLEAN MEMORY ##
+
+function clen_memory {
+echo "Ripristino script clean memory $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/05_ripristino_script.log
+echo "#!/bin/bash
+
+set -x
+
+sync; echo 1 > /proc/sys/vm/drop_caches
+sync; echo 2 > /proc/sys/vm/drop_caches" > /root/Script/clean_memory.sh
+}
+
+## FUNZIONE RIPRISTINO PULIZIA BCK ##
+
+function ripristino_bck {
+echo "Ripristino script pulizia bck $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/05_ripristino_script.log
+echo "#!/bin/bash
+
+set -x
+
+
+ssh -oStrictHostKeyChecking=no  admin@backup-server.sistemiesistemi.it "find /share/CACHEDEV1_DATA/MASTER-BCK -mtime +30  -exec rm {} \;" > /root/Script/pulizia.sh
+}
+
+## FUNZIONE RIPRISTINO PERMESSI ##
+
+function repair_perm {
+echo "Riparo permessi $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/05_ripristino_script.log
+chmod u+x /root/Script/bck.sh
+chmod u+x /root/Script/pulizia.sh
+chmod u+x /root/Script/clean_memory.sh
+chmod u+x /root/Script/rinnovo_certificati.sh
+chown -R thegod:thegod /home/thegod/Script
+chmod u+x /home/thegod/Script/Pulizia_VideoSorveglianza.sh
+}
+
+## FUNZIONE RIPRISTINO CRONTAB ##
+function ripristino_crontab {
+echo "RIPRISTINO CRONTAB $(date "+%d%m%Y %H:%M:%S")" >> /home/thegod/02_Ripristino.log
+echo "00 01 * * * /home/thegod/Script/Pulizia_VideoSorveglianza.sh pulisci >/dev/null 2>&1" | tee -a /var/spool/cron/crontabs/thegod
+echo "00 10 * * * /root/Script/bck.sh full > /dev/null 2>&1" | tee -a /var/spool/cron/crontabs/root
+echo "00 05 * * * /root/Script/pulizia.sh > /dev/null 2>&1" | tee -a /var/spool/cron/crontabs/root
+echo "00 * * * * /root/Script/clean_memory.sh > /dev/null 2>&1" | tee -a /var/spool/cron/crontabs/root
+echo "30 10 * * * /root/Script/rinnovo_certificati.sh renew > /var/log/certbot.log" | tee -a /var/spool/cron/crontabs/root
+}
+
+inizio_script
+crea_cartella
+check_utente
+ripristino_rinnovo_certificati
+ripristino_bck
+ripristino_pulizia_video
+clen_memory
+ripristino_bck
+repair_perm
+ripristino_crontab
